8654a79fbb1289d739dc52fcb03c69630461200d
Jason Matta <jasonmattaa@gmail.com>
2017-01-13 17:56:51 -0500

    clean code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,5 @@
 (function(){
 
-    /*
-     * Declaration of main angular module for this appllication.
-     *
-     * It is named turtleFacts and has no dependencies (hence the 
-     * empty array as the second argument)
-     */
     angular
         .module("myBookshelf", []);
 
